2300faac559a289296a7418787f1ab2b3e4dbe5c
R Reed <rreed@users.noreply.github.com>
2016-05-28 14:15:04 -0700

    Prize model/migration

---
+++ alembic/versions/4c468fae160c_add_prize_table.py
@@ -0,0 +1,37 @@
+"""Add prize table
+
+Revision ID: 4c468fae160c
+Revises: c303d333811
+Create Date: 2016-05-28 14:05:00.937034
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4c468fae160c'
+down_revision = 'c303d333811'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    op.create_table('prizes',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('title', sa.String()),
+        sa.Column('description', sa.String()),
+        sa.Column('quantity', sa.Integer(), nullable=False),
+        sa.Column('visible', sa.Boolean),
+        sa.Column('start', sa.DateTime(), nullable=False),
+        sa.Column('end', sa.DateTime(), nullable=False),
+        sa.Column('is_auction', sa.Boolean, default=False),
+        sa.Column('is_giveaway', sa.Boolean, default=False),
+        sa.Column('is_code', sa.Boolean, default=True),
+        sa.Column('entry_cost', sa.Float()),
+        sa.Column('game_id', sa.Integer, nullable=False),
+        sa.ForeignKeyConstraint(['game_id'], ['game.id'], name='fk_prize_games_game_id'),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk_prizes'))
+    )
+
+def downgrade():
+    op.drop_table('prizes')
---
+++ src/data/models/prize.py
@@ -0,0 +1,21 @@
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Boolean, Integer, String, Float, DateTime
+
+from ..base import Base
+from ..mixins import CRUDMixin
+
+class Prize(Base, CRUDMixin):
+    __tablename__ = 'prizes'
+
+    id = Column('id', Integer, primary_key=True)
+    title = Column('title', String())
+    description = Column('description', String())
+    quantity = Column('quantity', Integer, nullable=False)
+    visible = Column('visible', Boolean)
+    start = Column('start', DateTime)
+    end = Column('end', DateTime)
+    is_auction = Column('is_auction', Boolean, default=False)
+    is_giveaway = Column('is_auction', Boolean, default=False)
+    is_code = Column('is_auction', Boolean, default=True)
+    entry_cost = Column('entry_cost', Float)
+    game_id = Column('game_id', Integer, ForeignKey('game.id'), nullable=False)
