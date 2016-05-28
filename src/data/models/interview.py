4a228ac8cb203db4fd39dc65dda8810273e50cab
R Reed <rreed@users.noreply.github.com>
2016-05-28 14:54:38 -0700

    Some more minor cleanup

--- src/data/models/interview.py
+++ src/data/models/interview.py
@@ -9,7 +9,7 @@ class Interview(Base, CRUDMixin):
 
     id = Column('id', Integer, primary_key=True)
     name = Column('name', String())
-    description = Column('name', String())
+    description = Column('description', String())
     visible = Column('visible', Boolean, default=False)
-    game_id = Column('game_id', Integer, ForiegnKey('games.id'), nullable=False)
+    game_id = Column('game_id', Integer, ForeignKey('games.id'), nullable=False)
     developer = Column('developer', String())
