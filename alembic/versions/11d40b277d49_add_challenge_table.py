386826a00285fa5544721610e2151cfd08c49ffb
R Reed <rreed@users.noreply.github.com>
2016-05-28 17:53:46 -0700

    Challenges table/migration

---
+++ alembic/versions/11d40b277d49_add_challenge_table.py
@@ -0,0 +1,33 @@
+"""Add challenge table
+
+Revision ID: 11d40b277d49
+Revises: 4b170e1aa12e
+Create Date: 2016-05-28 17:48:09.210186
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '11d40b277d49'
+down_revision = '4b170e1aa12e'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    op.create_table('challenges',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String()),
+        sa.Column('description', sa.String()),
+        sa.Column('accepted', sa.Boolean, default=False),
+        sa.Column('visible', sa.Boolean, default=True),
+        sa.Column('bounty', sa.Float),
+        sa.Column('total', sa.Float),
+        sa.Column('user_id', sa.Integer, nullable=False),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], name='fk_challenges_users_user_id'),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk_interviews'))
+    )
+
+def downgrade():
+    op.drop_table('challenges')
---
+++ src/data/models/challenge.py
@@ -0,0 +1,17 @@
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Boolean, Integer, String
+
+from ..base import Base
+from ..mixins import CRUDMixin
+
+class Challenge(Base, CRUDMixin):
+    __tablename__ = 'challenges'
+
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String())
+    description = Column('description', String())
+    accepted = Column('accepted', Boolean, default=False)
+    visible = Column('visible', Boolean, default=True)
+    bounty = Column('bounty', Float)
+    total = Column('total', Float)
+    user_id = Column('user_id', Integer, ForeignKey('users.id'))
