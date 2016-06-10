77c8abd688decfa1d396af41a21fadb8c7cf1383
Daniel Ching <dan.t.ching@gmail.com>
2016-06-09 18:30:24 -0700

    updated per hour cost based on previous formula

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,9 @@ import os
 
 from sqlalchemy.engine.url import URL
 
+IGG_PARAM_RATE = 0.058
+IGG_PARAM_I_HR_COST = 10.00
+
 class DevConfig(object):
     ENV = 'dev'
     HOST = 'localhost:5000'
@@ -12,6 +15,9 @@ class DevConfig(object):
     SQLALCHEMY_SESSION_ARGS = {}
 
     APP_LOG_LEVEL = logging.DEBUG
+
+    IGG_PARAM_RATE = IGG_PARAM_RATE
+    IGG_PARAM_I_HR_COST = IGG_PARAM_I_HR_COST
 
 class TestConfig(object):
     ENV = 'test'
@@ -23,6 +29,10 @@ class TestConfig(object):
 
     APP_LOG_LEVEL = logging.DEBUG
 
+    IGG_PARAM_RATE = IGG_PARAM_RATE
+    IGG_PARAM_I_HR_COST = IGG_PARAM_I_HR_COST
+
+
 class ProdConfig(object):
     ENV = 'prod'
     HOST = 'somewhere'
@@ -32,6 +42,9 @@ class ProdConfig(object):
     SQLALCHEMY_SESSION_ARGS = {}
 
     APP_LOG_LEVEL = logging.ERROR
+
+    IGG_PARAM_RATE = IGG_PARAM_RATE
+    IGG_PARAM_I_HR_COST = IGG_PARAM_I_HR_COST
 
 config_dict = {
     'dev': DevConfig,
