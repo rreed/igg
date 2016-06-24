e05d60e2d36bd77c71316c3e4706ca8970e0a2e3
R Reed <rreed@users.noreply.github.com>
2016-06-23 19:46:13 -0700

    Add games to donation page

--- src/web/views/donate.py
+++ src/web/views/donate.py
@@ -2,7 +2,7 @@ import datetime
 
 from flask import render_template
 
-from ...data.models import Challenge, MarathonInfo, Prize
+from ...data.models import Challenge, Game, MarathonInfo, Prize
 from ...data.db import db
 
 def show():
@@ -12,7 +12,9 @@ def show():
     now = datetime.datetime.now()
     prizes = db.session.query(Prize).filter(Prize.start <= now).filter(Prize.end > now).all()
 
-    return render_template('donate/show.tmpl', challenges=challenges, prizes=prizes)
+    games = db.session.query(Game).all()
+
+    return render_template('donate/show.tmpl', challenges=challenges, prizes=prizes, games=games)
 
 def roi(amount):
     info = db.session.query(MarathonInfo).first()
